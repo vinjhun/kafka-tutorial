9674b016c8e15193a5871c784f333f3e40e985b1
vinjhun <jhunseng21@gmail.com>
2022-05-08 17:39:58 +0800

    first update

---
+++ kafka-basic/src/main/java/io/test/demo/kafka/ConsumerDemo.java
@@ -0,0 +1,86 @@
+package io.test.demo.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        Properties prop = initConfig();
+        receiveConsumer(prop);
+    }
+
+    private static Properties initConfig() {
+        Properties prop = new Properties();
+        prop.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        prop.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        prop.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        prop.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-second-application");
+        prop.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");  // none/earliest/latest
+
+        return prop;
+    }
+
+    private static void receiveConsumer(Properties prop) {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(prop);
+
+        //add thread
+        final Thread mainThread = Thread.currentThread();
+
+        //add shutdownhook
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                log.info("Detected a shutdown .Lets exit by calling consumer.wakeup()");
+                consumer.wakeup();  //this will cause poll to cause exception
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+
+        try {
+            //subscribe to a topic
+            consumer.subscribe(Collections.singletonList("demo_java"));
+
+            //poll for new data
+            while(true) {
+                log.info("Polling");
+
+                ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key : " + record.key() + " Value : " + record.value());
+                    log.info("Partition : " + record.partition() + " Offsets : " + record.offset());
+                }
+
+            }
+        } catch (WakeupException e) {
+            log.info(" Wake up exception");
+            //ignore as this is an expected exception
+        } catch (Exception e) {
+            log.error("Unexpected exception " + e.getMessage());
+        } finally {
+            consumer.close();   //this will also commit the offset
+            log.info("Close Gracefully");
+        }
+
+    }
+}
---
+++ kafka-basic/src/main/java/io/test/demo/kafka/ProducerDemo.java
@@ -0,0 +1,102 @@
+package io.test.demo.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    //kafka-topics --bootstrap-server localhost:9092 --create --topic demo_java --partitions 3 --replication-factor 1
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello World!");
+
+        Properties prop = initConfig();
+        //sendHello(prop);
+        sendCallback(prop);
+    }
+
+    private static Properties initConfig() {
+        Properties prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return prop;
+    }
+
+    private static void sendHello(Properties properties) {
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create data
+        ProducerRecord<String, String > producerRecord =
+                new ProducerRecord<>("demo_java", "hello_word");
+
+        //send data - asynchronous
+        producer.send(producerRecord);
+
+        //flush and close producer
+        //flush data - synchronous
+        producer.flush();
+
+        //close
+        producer.close();
+    }
+
+    private static void sendCallback(Properties properties) {
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+
+        //send data - asynchronous
+        for (int i = 0; i < 10; i++) {
+
+            /*
+            * ProducerRecord<String, String > producerRecord =
+                    new ProducerRecord<>("demo_java", call_back" + i);
+            * */
+
+            //create data in key value
+            ProducerRecord<String, String > producerRecord =
+                    new ProducerRecord<>("demo_java", "id_" + i, "call_back" + i);
+
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception e) {
+                    if (e == null) {
+                        log.info("Received new metadata / \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Key: " + producerRecord.key() + "\n" +
+                                "Offset: " + metadata.hasOffset() + "\n" +
+                                "Timestamp:" + metadata.timestamp() + "\n");
+                    } else {
+                        log.error(e.getMessage());
+                    }
+                }
+            });
+
+            //sticky partitioner will send msg in batches if the msg is send fast enough
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        //flush and close producer
+        //flush data - synchronous
+        producer.flush();
+
+        //close
+        producer.close();
+    }
+}
